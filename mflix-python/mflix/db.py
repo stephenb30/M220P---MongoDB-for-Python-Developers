3403eac25436f477b3513fcdbbd2d1b7cc776caf
stephenb30 <stephennbreen@gmail.com>
2022-08-11 12:47:50 +0100

    chpt3

--- mflix-python/mflix/db.py
+++ mflix-python/mflix/db.py
@@ -279,6 +279,28 @@ def get_movie(id):
                 "$match": {
                     "_id": ObjectId(id)
                 }
+            },
+            {'$lookup': {
+                    'from': 'comments',
+                    'let': {
+                        'id': '$_id'
+                    },
+                    'pipeline': [
+                        {
+                            '$match': {
+                                '$expr': {
+                                    '$eq': [
+                                        '$movie_id', '$$id'
+                                    ]
+                                }
+                            }
+                        },
+                        {
+                            '$sort': {'date': -1}
+                        }
+                    ],
+                    'as': 'comments'
+                }
             }
         ]
 
@@ -340,7 +362,15 @@ def add_comment(movie_id, user, comment, date):
     """
     # TODO: Create/Update Comments
     # Construct the comment document to be inserted into MongoDB.
-    comment_doc = { "some_field": "some_value" }
+
+
+
+    comment_doc = { "name": user.name,
+                    "email": user.email,
+                    "movie_id": ObjectId(movie_id) ,
+                    "text": comment,
+                    "date": date
+                    }
     return db.comments.insert_one(comment_doc)
 
 
@@ -354,10 +384,9 @@ def update_comment(comment_id, user_email, text, date):
     # Use the user_email and comment_id to select the proper comment, then
     # update the "text" and "date" of the selected comment.
     response = db.comments.update_one(
-        { "some_field": "some_value" },
-        { "$set": { "some_other_field": "some_other_value" } }
+        { "_id": ObjectId(comment_id), "email": user_email },
+        { "$set": { "text": text, "date": date } }
     )
-
     return response
 
 
@@ -376,7 +405,7 @@ def delete_comment(comment_id, user_email):
 
     # TODO: Delete Comments
     # Use the user_email and comment_id to delete the proper comment.
-    response = db.comments.delete_one( { "_id": ObjectId(comment_id) } )
+    response = db.comments.delete_one( { "_id": ObjectId(comment_id), "email": user_email } )
     return response
 
 
@@ -552,9 +581,21 @@ def most_active_commenters():
     """
     # TODO: User Report
     # Return the 20 users who have commented the most on MFlix.
-    pipeline = []
+    pipeline = [
+        {"$match": {"email": {"$exists": True}}},
 
-    rc = db.comments.read_concern # you may want to change this read concern!
+        {"$group": {
+                    "_id": "$email",
+                    "count": {"$sum": 1}
+                    }
+        },
+
+        {"$sort": {"count": -1}},
+        
+        {"$limit": 20}
+    ]
+
+    rc = ReadConcern("majority") # you may want to change this read concern!
     comments = db.comments.with_options(read_concern=rc)
     result = comments.aggregate(pipeline)
     return list(result)
